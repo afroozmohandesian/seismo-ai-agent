3e80a3178e7c587df7030ec1bb1460afae7bbadc
AfroozGit <mohandesian66@gmail.com>
2026-05-08 17:31:55 +0200

    Add Bayesian belief update mechanism

---
+++ belief/updates/bayesian_update.py
@@ -0,0 +1,42 @@
+class BayesianUpdater:
+
+    @staticmethod
+    def update(
+        prior_mean,
+        prior_variance,
+        observation,
+        observation_variance,
+    ):
+
+        posterior_mean = (
+            (
+                observation_variance * prior_mean
+                +
+                prior_variance * observation
+            )
+            /
+            (
+                prior_variance
+                +
+                observation_variance
+            )
+        )
+
+        posterior_variance = (
+            (
+                prior_variance
+                *
+                observation_variance
+            )
+            /
+            (
+                prior_variance
+                +
+                observation_variance
+            )
+        )
+
+        return (
+            posterior_mean,
+            posterior_variance,
+        )
